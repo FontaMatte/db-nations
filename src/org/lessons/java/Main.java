e20ce7a526fbc0984969ffe8c45ded326fb5b2b1
FontaMatte <matteo.fontanel@gmail.com>
2023-06-16 17:03:28 +0200

    Bonus Completed

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -34,13 +34,64 @@ public class Main {
                         String country = rs.getString(2);
                         String region = rs.getString(3);
                         String continent = rs.getString(4);
-                        System.out.println(country_id + " - " + country + " - " + region + " - " + continent);
+                        System.out.println("ID: " + country_id + " - " + country + " - " + region + " - " + continent);
                     }
                 }
             }
+
+            System.out.println("choose a country ID among the filtered ones: ");
+            int countryId = Integer.parseInt(scanner.nextLine());
+
+            String languageSql = """
+                SELECT `language`
+                FROM `languages`
+                JOIN `country_languages`
+                ON languages.language_id = country_languages.language_id
+                WHERE country_id = ?;""";
+            try (PreparedStatement languagePs = connection.prepareStatement(languageSql)) {
+                languagePs.setInt(1, countryId);
+                try (ResultSet languageRs = languagePs.executeQuery()) {
+                    System.out.println("Languages speak: ");
+                    while (languageRs.next()) {
+                        String language = languageRs.getString(1);
+                        System.out.println(" - " + language);
+                    }
+                }
+            }
+
+            String statsSql = """
+                SELECT *
+                FROM `countries`
+                JOIN `country_stats` ON countries.country_id = country_stats.country_id
+                WHERE countries.country_id = ?
+                AND country_stats.year = (
+                    SELECT MAX(year)
+                    FROM `country_stats`
+                    WHERE country_id = ?
+                );""";
+            try (PreparedStatement statsPs = connection.prepareStatement(statsSql)) {
+                statsPs.setInt(1, countryId);
+                statsPs.setInt(2, countryId);
+                try (ResultSet statsRs = statsPs.executeQuery()) {
+                    System.out.println("Most recently statistics: ");
+                    if (statsRs.next()) {
+                        long population = statsRs.getLong("country_stats.population");
+                        long gdp = statsRs.getLong("country_stats.gdp");
+                        int year = statsRs.getInt("country_stats.year");
+
+                        System.out.println("Population: " + population);
+                        System.out.println("GDP: " + gdp);
+                        System.out.println("Year: " + year);
+                    } else {
+                        System.out.println("No statistics found for the ID " + countryId);
+                    }
+                }
+            }
+
         } catch (SQLException e) {
             System.out.println("Unable to connect to database");
             e.printStackTrace();
         }
     }
 }
+
